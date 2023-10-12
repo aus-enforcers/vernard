58b859a846463c776a71060b9e82a5addf44edfc
spiltcoffee <spiltcoffee@users.noreply.github.com>
2023-10-12 15:44:22 +1100

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics;
+using Windows.UI.ViewManagement;
+using WinRT.Interop;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace vernard
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            AppWindow appWindow = AppWindow.GetFromWindowId(wndId);
+
+            appWindow.Resize(new SizeInt32(540, 560));
+
+            OverlappedPresenter appPresenter = appWindow.Presenter as OverlappedPresenter;
+            appPresenter.IsAlwaysOnTop = true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# vernard
+
+Vernard, the VR Stopwatch
